48a66b1980697e6b228e99c3527e3131f2b7562d
Schuppel1 <sven.schu.mobil@gmail.com>
2022-11-21 20:58:37 +0100

    basic Rest settings

--- doZitat/src/main/java/org/schuppel/quarkus/doZitat/Quote.java
+++ doZitat/src/main/java/org/schuppel/quarkus/doZitat/Quote.java
@@ -1,4 +1,27 @@
 package org.schuppel.quarkus.doZitat;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class Quote {
+
+    public int id;
+    public String quote;
+    public String profName;
+    public String course;
+    //private DateFormat dateFormat  = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    public Date created;
+    public String creator;
+
+    public Quote(int id, String quote, String profName, String course, String creator) {
+        this.id = id;
+        this.quote = quote;
+        this.profName = profName;
+        this.course = course;
+        this.creator = creator;
+        this.created = new Date();
+        //System.out.println(dateFormat.format(created));
+    }
 }
--- doZitat/src/main/java/org/schuppel/quarkus/doZitat/QuoteResource.java
+++ doZitat/src/main/java/org/schuppel/quarkus/doZitat/QuoteResource.java
@@ -2,15 +2,38 @@ package org.schuppel.quarkus.doZitat;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.Optional;
 
 @Path("/quotes")
 public class QuoteResource {
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Quote> getAllQuotes() {
+        return List.of(
+                new Quote(1,"ich bin test1", "Prof1", "IT1","User1"),
+                new Quote(2,"ich bin test2", "Prof2", "WEBIT","User2"),
+                new Quote(3,"ich bin test3", "Prof3", "PHP","User1"),
+                new Quote(4,"ich bin test4", "Prof1", "IT2","User1"),
+                new Quote(5,"ich bin test5", "Prof2", "WEBIT3","User2")
+        );
+    }
+
+    @GET
+    @Path("/count")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public int getNumberOfAllQuotes() {
+        return getAllQuotes().size();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Optional<Quote> getQuote(@PathParam("id") int id) {
+        return getAllQuotes().stream().filter(quote -> quote.id == id).findFirst();
     }
 }
