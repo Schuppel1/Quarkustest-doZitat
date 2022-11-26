dd86787b187897f7e935f555d1626b5f02ad3039
Schuppel1 <sven.schu.mobil@gmail.com>
2022-11-26 16:41:13 +0100

    remove sql dump

--- doZitat/src/test/java/org/schuppel/quarkus/doZitat/QuoteResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.schuppel.quarkus.doZitat;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class QuoteResourceIT extends QuoteResourceTest {
-    // Execute the same tests but in packaged mode.
-}
